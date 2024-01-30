07f99578065a753878943e9db48cabbdde8c084b
fr30 <fjakub.iia@gmail.com>
2024-01-30 15:50:56 +0100

    Add minibatching and uniform sampling training

---
+++ src/sampler.py
@@ -0,0 +1,167 @@
+import torch
+from abc import abstractmethod
+
+
+class GenericSampler:
+    def __init__(
+        self,
+        dataset,
+        batch_size=1,
+        num_nbr_layers=2,
+    ):
+        data = dataset.data
+        self.train_mask = data.train_mask
+        self.test_mask = data.test_mask
+        self.val_mask = data.val_mask
+        self.batch_size = batch_size
+        self.node_features = data.x
+        self.labels = data.y
+        self.num_nodes = data.num_nodes
+        self.num_train = data.train_mask.sum().item()
+        self.num_val = data.val_mask.sum().item()
+        self.num_test = data.test_mask.sum().item()
+        self.num_nbr_layers = num_nbr_layers
+        self.adj = torch.sparse_coo_tensor(
+            data.edge_index, torch.ones(data.edge_index.shape[1])
+        )
+
+    def train_iter(self):
+        return self._get_data_iter(self.train_mask.nonzero().view(-1))
+
+    def test_iter(self):
+        return self._get_data_iter(self.test_mask.nonzero().view(-1))
+
+    def val_iter(self):
+        return self._get_data_iter(self.val_mask.nonzero().view(-1))
+
+    def _get_data_iter(self, all_nodes):
+        node_perm = torch.randperm(all_nodes.shape[0])
+        shuffled_nodes = all_nodes[node_perm]
+        nodes_iter = iter(torch.split(shuffled_nodes, self.batch_size))
+
+        def data_gen():
+            for nodes in nodes_iter:
+                nodes_layerwise, adj_layerwise = self._get_neighbourhoods(nodes)
+                final_nodes = nodes_layerwise[-1].long()
+                features = self.node_features[final_nodes]
+
+                # Map the nodes to their indices in the final_nodes tensor
+                scaled_nodes = map_elem_to_id(torch.cat([nodes, final_nodes]))
+                scaled_nodes = scaled_nodes[: nodes.shape[0]]
+
+                yield scaled_nodes, features, adj_layerwise, self.labels[nodes]
+
+        return data_gen()
+
+    @abstractmethod
+    def _get_neighbourhoods(self, start_nodes):
+        pass
+
+
+# Sampler that returns full neighbourhood
+class IdentitySampler(GenericSampler):
+    def _get_neighbourhoods(self, start_nodes):
+        nodes_layerwise = []
+        adj_layerwise = []
+        nodes = start_nodes.float()
+
+        for _ in range(self.num_nbr_layers + 1):
+            nodes_layerwise.append(nodes)
+            next_nodes, scaled_next_adj = self._get_single_neighbourhood(nodes)
+
+            nodes_mask = torch.zeros(self.num_nodes)
+            nodes_mask[next_nodes.long()] = 1
+            nodes_mask[nodes.long()] = 1
+            nodes = nodes_mask.nonzero().reshape(-1).float()
+
+            adj_layerwise.append(scaled_next_adj)
+
+        nodes_layerwise.append(nodes)
+
+        return nodes_layerwise, adj_layerwise
+
+    # Function that creates neighourhood from given start nodes.
+    # It returns nodes from the next layer as well as the scaled adjacency matrix
+    def _get_single_neighbourhood(self, nodes):
+        onehot_nodes = get_onehot_sparse(nodes, self.adj.shape)
+        sliced_adj = onehot_nodes @ self.adj
+
+        next_nodes_mask = torch.zeros(self.num_nodes)
+        next_nodes_mask[sliced_adj.indices().view(-1)] = 1
+        next_nodes = next_nodes_mask.nonzero().reshape(-1).float()
+
+        scaled_adj = map_elem_to_id(sliced_adj.indices())
+
+        return next_nodes, scaled_adj
+
+
+# Sample that uniformly samples nodes from previous neighbourhoods
+class UniformSampler(GenericSampler):
+    def __init__(self, k, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.k = k
+
+    def _get_neighbourhoods(self, start_nodes):
+        nodes_layerwise = []
+        adj_layerwise = []
+        nodes = start_nodes.float()
+
+        for _ in range(self.num_nbr_layers + 1):
+            nodes_layerwise.append(nodes)
+            next_nodes, scaled_next_adj = self._get_single_neighbourhood(nodes)
+
+            nodes_mask = torch.zeros(self.num_nodes)
+            nodes_mask[next_nodes.long()] = 1
+            nodes_mask[nodes.long()] = 1
+            nodes = nodes_mask.nonzero().reshape(-1).float()
+
+            adj_layerwise.append(scaled_next_adj)
+
+        nodes_layerwise.append(nodes)
+        return nodes_layerwise, adj_layerwise
+
+    # Function that creates neighourhood from given start nodes.
+    # It returns nodes from the next layer as well as the scaled adjacency matrix
+    def _get_single_neighbourhood(self, nodes):
+        onehot_nodes = get_onehot_sparse(nodes, self.adj.shape)
+        left_sliced_adj = onehot_nodes @ self.adj
+        next_nodes = left_sliced_adj.indices().float().reshape(-1)
+
+        # Get nodes from the next layer, excluding nodes from current layer
+        nodes_mask = torch.zeros(self.num_nodes).bool()
+        nodes_mask[nodes.long()] = 1
+        next_nodes_mask = torch.zeros(self.num_nodes).bool()
+        next_nodes_mask[next_nodes.long()] = 1
+
+        next_nodes_mask = next_nodes_mask & ~nodes_mask
+        next_layer_nodes = next_nodes_mask.nonzero().reshape(-1).float()
+
+        # Uniformly sample which nodes to consider in the next layer
+        sampled_nodes = self._sample_tensor(next_layer_nodes)
+        onehot_next = get_onehot_sparse(sampled_nodes, self.adj.shape)
+        sliced_adj = left_sliced_adj @ onehot_next
+        scaled_adj = map_elem_to_id(sliced_adj.indices())
+
+        return sampled_nodes, scaled_adj
+
+    def _sample_tensor(self, x):
+        perm = torch.randperm(x.size(0))
+        idx = perm[: self.k]
+        return x[idx]
+
+
+# Function that maps the elements of the tensor to their indices in the uniq() tensor
+# Example: [1, 2, 3, 1, 2] -> [0, 1, 2, 0, 1]
+def map_elem_to_id(keys):
+    num_elems = keys.max() + 1
+    map = torch.zeros(num_elems, dtype=torch.long)
+    map[keys] = 1
+    map = map.cumsum(dim=0) - 1
+    return map[keys]
+
+
+def get_onehot_sparse(elems, shape):
+    onehot_i = elems.repeat((2, 1))
+    onehot_v = torch.ones_like(elems)
+    onehot_sparse = torch.sparse_coo_tensor(onehot_i, onehot_v, shape)
+    return onehot_sparse
---
+++ train_minibatch.py
@@ -0,0 +1,96 @@
+import hydra
+import time
+import torch
+import torch.nn.functional as F
+import torch_geometric.transforms as T
+
+from torch_geometric.datasets import Planetoid
+from torch_geometric.logging import log
+from src.models import GCN
+from src.sampler import IdentitySampler
+
+
+@hydra.main(version_base=None, config_path="configs", config_name="minibatch")
+def main(cfg):
+    if cfg.seed is not None:
+        torch.manual_seed(cfg.seed)
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    dataset = Planetoid(
+        root="data", name="Cora", split="full", transform=T.NormalizeFeatures()
+    )
+    # loader = BatchGraphLoader(dataset, batch_size=cfg.batch_size, num_neighbor_layers=2)
+    sampler = IdentitySampler(dataset, batch_size=cfg.batch_size)
+    model = GCN(
+        in_channels=dataset.num_features,
+        hidden_channels=cfg.hidden_channels,
+        out_channels=dataset.num_classes,
+    ).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
+
+    def train():
+        model.train()
+        running_loss = 0.0
+        running_acc = 0
+        for local_nodes, x, adj, y in sampler.train_iter():
+            local_nodes = local_nodes.to(device)
+            x = x.to(device)
+            adj = [a.to(device) for a in adj]
+            y = y.to(device)
+
+            optimizer.zero_grad()
+            out = model(x, adj)
+            loss = F.cross_entropy(out[local_nodes], y)
+            loss.backward()
+            optimizer.step()
+
+            running_acc += int((out[local_nodes].argmax(dim=-1) == y).sum())
+            running_loss += float(loss)
+        return running_loss * cfg.batch_size / sampler.num_train
+
+    @torch.no_grad()
+    def test_full():
+        data = dataset.data.to(device)
+        model.eval()
+        pred = model(data.x, data.edge_index).argmax(dim=-1)
+        accs = []
+        for mask in [data.train_mask, data.val_mask, data.test_mask]:
+            accs.append(int((pred[mask] == data.y[mask]).sum()) / int(mask.sum()))
+        return accs
+
+    @torch.no_grad()
+    def test_minibatch():
+        model.eval()
+        accs = []
+        data_iters = [
+            (sampler.train_iter(), sampler.num_train),
+            (sampler.val_iter(), sampler.num_val),
+            (sampler.test_iter(), sampler.num_test),
+        ]
+        for iter, iter_len in data_iters:
+            correct = 0
+            for local_nodes, x, adj, y in iter:
+                local_nodes = local_nodes.to(device)
+                x = x.to(device)
+                adj = [a.to(device) for a in adj]
+                y = y.to(device)
+
+                pred = model(x, adj)[local_nodes].argmax(dim=-1)
+                correct += int((pred == y).sum())
+            accs.append(correct / iter_len)
+
+        return accs
+
+    times = []
+    test = test_full if cfg.test_full else test_minibatch
+    for epoch in range(1, cfg.epochs + 1):
+        start = time.time()
+        loss = train()
+        train_acc, val_acc, test_acc = test()
+        log(Epoch=epoch, Loss=loss, Train=train_acc, Val=val_acc, Test=test_acc)
+        times.append(time.time() - start)
+    print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
+
+
+if __name__ == "__main__":
+    main()
